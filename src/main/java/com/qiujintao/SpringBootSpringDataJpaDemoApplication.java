513619288e2255a64c188a281e677a46b64f706f
alanHarper123 <1531508001@qq.com>
2018-07-21 22:10:18 +0800

    created by eclipse STS plugin

---
+++ src/main/java/com/qiujintao/SpringBootSpringDataJpaDemoApplication.java
@@ -0,0 +1,12 @@
+package com.qiujintao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringDataJpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpringDataJpaDemoApplication.class, args);
+	}
+}
